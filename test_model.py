97c4cda86a70ee097b18d453b34809feb6d3f581
srivastavashantam <133944575+srivastavashantam@users.noreply.github.com>
2026-03-30 07:16:16 +0530

    Delete test_model.py

--- test_model.py
+++
@@ -1,6 +0,0 @@
-from src.models.tcn_model import load_tcn_model
-model, config = load_tcn_model()
-print('Model loaded!')
-print(f'Threshold : {config["threshold"]}')
-print(f'Channels  : {config["n_channels"]}')
-print(f'Timesteps : {config["n_timesteps"]}')
